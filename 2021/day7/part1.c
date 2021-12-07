7b29b2ec0644bb25c6cbff52e7d0ff1581132598
Luukas Pörtfors <lajp@iki.fi>
2021-12-07 07:31:54 +0200

    Add (bruteforce) solutions for day 7
    This is their own fault for making the probles too easy!!!

---
+++ 2021/day7/part1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int main() {
+    int minval, maxval, total, sum;
+    total = maxval = 0;
+    sum = minval = INT_MAX;
+    int values[1000];
+    char* line = NULL;
+    size_t s = 128;
+    getline(&line, &s, stdin);
+    const char* in;
+    for(in = strtok(line, ","); in && *in; ++total) {
+        int num = strtol(in, NULL, 10);
+        values[total] = num;
+        if(num < minval) {
+            minval = num;
+        } else if(num > maxval) {
+            maxval = num;
+        }
+        in = strtok(NULL, ",");
+    }
+    free(line);
+    for(int i = minval; i < maxval; ++i) {
+        int fsum = 0;
+        for(int j = 0; j < total; ++j) {
+            fsum += abs(i-values[j]);
+        }
+        if(fsum < sum) {
+            sum = fsum;
+        }
+    }
+    printf("Answer: %d\n", sum);
+    return 0;
+}
