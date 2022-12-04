b8e7863d33b96a8639aad31251abec94debf9c09
Luukas Pörtfors <lajp@iki.fi>
2022-12-04 09:16:08 +0200

    feat(2022): add solutions for day04
    I didn't know pascal had sscanf, but without it I would've been in
    trouble :))

---
+++ 2022/day04/part2.pas
@@ -0,0 +1,17 @@
+program part1;
+uses sysutils;
+var a1, a2, b1, b2, ans: int64;
+var ip: string;
+
+begin
+    ans := 0;
+    while not eof do
+    begin
+        readln(ip);
+        sscanf(ip, '%d-%d,%d-%d', [@a1, @a2, @b1, @b2]);
+        if ((a1 >= b1) and (a1 <= b2)) or ((a2 >= b1) and (a2 <= b2)) or
+            ((b1 >= a1) and (b1 <= a2)) or ((b2 >= a1) and (b2 <= a2)) then
+            inc(ans);
+    end;
+    writeln(ans);
+end.
