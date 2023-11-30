1c65fbd222c54e4567456b7bcbbf2aafcbef4998
Luukas Pörtfors <lajp@iki.fi>
2023-11-30 22:18:20 +0200

    feat(2023): add templates

---
+++ 2023/template.rs
@@ -0,0 +1,13 @@
+fn part1(input: String) -> String {
+    todo!()
+}
+
+fn part2(input: String) -> String {
+    todo!()
+}
+
+fn main() {
+    let input: String = include_str!("../input/DAYNUM").to_string();
+    println!("PART1: {}", part1(input.clone()));
+    println!("PART2: {}", part2(input.clone()));
+}
---
+++ 2023/today.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+TODAY="$(date +%d)"
+FILENAME="src/day$TODAY.rs"
+cp template.rs "$FILENAME"
+sed -i "s/DAYNUM/$TODAY/g" "$FILENAME"
+cat template.toml | sed "s/DAYNUM/$TODAY/g" >> Cargo.toml
+echo "Enter input:"
+cat > "input/$TODAY"
+cargo watch -x "run -- --bin day$TODAY"
