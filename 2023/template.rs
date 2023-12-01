be238912cec47da1d8d288fffb50895f35d4e89e
Luukas Pörtfors <lajp@iki.fi>
2023-12-01 08:02:45 +0200

    refactor(2023): improve template

--- 2023/template.rs
+++ 2023/template.rs
@@ -1,13 +1,15 @@
-fn part1(input: String) -> String {
+use std::fmt::Display;
+
+fn part1(input: String) -> impl Display {
     todo!()
 }
 
-fn part2(input: String) -> String {
+fn part2(input: String) -> impl Display {
     todo!()
 }
 
 fn main() {
     let input: String = include_str!("../input/DAYNUM").to_string();
     println!("PART1: {}", part1(input.clone()));
-    println!("PART2: {}", part2(input.clone()));
+    println!("PART2: {}", part2(input));
 }
