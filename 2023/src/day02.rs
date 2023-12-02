fabe3c10c724dce07793c0591caee1ef6f65aa25
Luukas Pörtfors <lajp@iki.fi>
2023-12-02 14:29:58 +0200

    refactor(2023): cleanup day02
    From now on I'll be using regex for parsing :D

--- 2023/src/day02.rs
+++ 2023/src/day02.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+use std::cmp::max;
 use std::fmt::Display;
 
 fn part1(input: String) -> impl Display {
@@ -6,49 +8,35 @@ fn part1(input: String) -> impl Display {
     let bluemax = 14;
     let mut ans = 0;
 
-    for line in input.lines() {
-        let sp = line.split(' ').collect::<Vec<_>>();
-        let colors = line.split(';').collect::<Vec<_>>();
-        let id: i32 = sp[1]
-            .chars()
-            .take_while(|c| c.is_numeric())
-            .collect::<String>()
-            .parse()
-            .unwrap();
+    for (id, line) in input.lines().enumerate() {
         let mut possible = true;
-
-        for cs in colors {
-            let cs = cs
-                .strip_prefix(&format!("Game {}: ", id))
-                .unwrap_or(cs)
-                .split(',')
-                .collect::<Vec<_>>();
-            for c in cs {
-                let c = c.trim().split(' ').collect::<Vec<_>>();
-                let num = c[0].parse::<i32>().unwrap();
-                match c[1] {
-                    "blue" => {
-                        if num > bluemax {
-                            possible = false
-                        }
+        for cap in Regex::new(r"(\d+) (blue|red|green)")
+            .unwrap()
+            .captures_iter(line)
+        {
+            let num = cap[1].parse::<i32>().unwrap();
+            match &cap[2] {
+                "red" => {
+                    if num > redmax {
+                        possible = false;
                     }
-                    "red" => {
-                        if num > redmax {
-                            possible = false
-                        }
-                    }
-                    "green" => {
-                        if num > greenmax {
-                            possible = false
-                        }
-                    }
-                    _ => unreachable!(),
                 }
+                "green" => {
+                    if num > greenmax {
+                        possible = false;
+                    }
+                }
+                "blue" => {
+                    if num > bluemax {
+                        possible = false;
+                    }
+                }
+                _ => unreachable!(),
             }
         }
 
         if possible {
-            ans += id
+            ans += id + 1
         }
     }
     ans
@@ -61,45 +49,25 @@ fn part2(input: String) -> impl Display {
         let mut greenmax = 0;
         let mut bluemax = 0;
 
-        let sp = line.split(' ').collect::<Vec<_>>();
-        let colors = line.split(';').collect::<Vec<_>>();
-        let id: i32 = sp[1]
-            .chars()
-            .take_while(|c| c.is_numeric())
-            .collect::<String>()
-            .parse()
-            .unwrap();
-
-        for cs in colors {
-            let cs = cs
-                .strip_prefix(&format!("Game {}: ", id))
-                .unwrap_or(cs)
-                .split(',')
-                .collect::<Vec<_>>();
-
-            for c in cs {
-                let c = c.trim().split(' ').collect::<Vec<_>>();
-                let num = c[0].parse::<i32>().unwrap();
-                match c[1] {
-                    "blue" => {
-                        if num > bluemax {
-                            bluemax = num
-                        }
-                    }
-                    "red" => {
-                        if num > redmax {
-                            redmax = num
-                        }
-                    }
-                    "green" => {
-                        if num > greenmax {
-                            greenmax = num
-                        }
-                    }
-                    _ => unreachable!(),
+        for cap in Regex::new(r"(\d+) (blue|red|green)")
+            .unwrap()
+            .captures_iter(line)
+        {
+            let num = cap[1].parse::<i32>().unwrap();
+            match &cap[2] {
+                "red" => {
+                    redmax = max(redmax, num);
                 }
+                "green" => {
+                    greenmax = max(greenmax, num);
+                }
+                "blue" => {
+                    bluemax = max(bluemax, num);
+                }
+                _ => unreachable!(),
             }
         }
+
         ans += bluemax * greenmax * redmax;
     }
     ans
