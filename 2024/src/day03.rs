5606d6a718ec2dcb9c9d2f7daf64678b309e011e
Luukas Pörtfors <lajp@iki.fi>
2024-12-04 20:01:30 +0200

    refactor(2024/day03): minor cleanup

--- 2024/src/day03.rs
+++ 2024/src/day03.rs
@@ -11,23 +11,17 @@ fn part1(input: &str) -> impl std::fmt::Display {
 }
 
 fn part2(input: &str) -> impl std::fmt::Display {
-    // NOTE: ()() is a hack in order to make the amount of capture groups equal
-    // so that Captures::extract can be used
-    let re = regex::Regex::new(r"mul\((\d+),(\d+)\)|()()do\(\)|()()don't\(\)").unwrap();
+    let re = regex::Regex::new(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)").unwrap();
 
     re.captures_iter(input)
-        .fold((0, true), |(sum, enable), caps| {
-            let (s, [n1, n2]) = caps.extract();
-
-            match (s, enable) {
-                ("do()", _) => (sum, true),
-                ("don't()", _) => (sum, false),
-                (_, true) => (
-                    sum + n1.parse::<i64>().unwrap() * n2.parse::<i64>().unwrap(),
-                    enable,
-                ),
-                (_, false) => (sum, enable),
-            }
+        .fold((0, true), |(sum, enable), caps| match (&caps[0], enable) {
+            ("do()", _) => (sum, true),
+            ("don't()", _) => (sum, false),
+            (_, true) => (
+                sum + caps[1].parse::<i64>().unwrap() * caps[2].parse::<i64>().unwrap(),
+                enable,
+            ),
+            (_, false) => (sum, enable),
         })
         .0
 }
